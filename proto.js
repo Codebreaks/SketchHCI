f98c4891287a524ff9f2a0f732b28a595d455648
LjXu666 <xu_lingjie@yahoo.com>
2017-11-30 02:18:17 -0600

    Two Prototypes

--- proto.js
+++ proto.js
@@ -1,19 +1,26 @@
 var clickX_simple = new Array();
 var clickY_simple = new Array();
+var strokex = new Array();
+var strokey = new Array();
+var removeX = new Array();
+var removeY = new Array();
 var clickDrag_simple = new Array();
 var paint_simple;
 var canvas_simple;
 var context_simple;
 var canvasWidth = 700;
 var canvasHeight = 500;
-var colorBlack = "#110101";
-var colorRed = "#FF3333";
 var colorPurple = "#cb3594";
 var colorGreen = "#659b41";
 var colorYellow = "#ffcf33";
 var colorBrown = "#986928";
-var colorOrange = "#FF8300";
+var colorBlue = "#0000FF";
+var colorRed = "#FF0000";
+var colorBlack = "#000000";
 var eraser = '#ffffff';
+
+var undoLog = 0;
+var redoLog = 0;
 
 var curColor_simpleColors = colorPurple;
 var clickColor = new Array();
@@ -29,6 +36,8 @@ var curTool = "color";
 function prepareSimpleCanvas()
 {
 
+	savedImages= [];
+	removedImages= [];
 	// Create the canvas (Neccessary for IE because it doesn't know what a canvas element is)
 	var canvasDiv = document.getElementById('protoCanvas');
 	canvas_simple = document.createElement('canvas');
@@ -40,25 +49,36 @@ function prepareSimpleCanvas()
 		canvas_simple = G_vmlCanvasManager.initElement(canvas_simple);
 	}
 	context_simple = canvas_simple.getContext("2d");
-	context_simple.fillStyle = "#FBF8F8";
-	context_simple.fillRect(0, 0, canvas_simple.canvasWidth, canvas_simple.canvasHeight);
 
+
+	function getMousePos(canvas, evt) {
+		var rect = canvas.getBoundingClientRect();
+		return {
+  			x: evt.clientX - rect.left,
+  			y: evt.clientY - rect.top
+		};
+	}
 	// Add mouse events
 	// ----------------
 	canvas_simple.addEventListener("mousedown", function(e)
 	{
+		var pos = getMousePos(canvasDiv,e)
 		// Mouse down location
-		var mouseX = e.pageX - this.offsetLeft;
-		var mouseY = e.pageY - this.offsetTop;
-
+		var mouseX = pos.x - this.offsetLeft;
+		var mouseY = pos.y - this.offsetTop;
+		strokex = [];
+		strokey = [];
 		paint_simple = true;
 		addClickSimple(mouseX, mouseY, false);
+		saveStroke(mouseX,mouseY);
 		redrawSimple();
 	});
 
 	canvas_simple.addEventListener("mousemove", function(e){
+		var pos = getMousePos(canvasDiv,e)
 		if(paint_simple){
-			addClickSimple(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+			addClickSimple(pos.x - this.offsetLeft, pos.y - this.offsetTop, true);
+			saveStroke(pos.x - this.offsetLeft, pos.y - this.offsetTop);
 			redrawSimple();
 		}
 	});
@@ -80,6 +100,7 @@ function prepareSimpleCanvas()
 		clickColor = new Array();
 		clearCanvas_simple();
 	});
+    
 	//choose colors
 	grabOrange.addEventListener("mousedown", function(e){
 		curColor_simpleColors = colorOrange;
@@ -99,6 +120,9 @@ function prepareSimpleCanvas()
 	grabYellow.addEventListener("mousedown", function(e){
 		curColor_simpleColors = colorYellow;
 	});
+	grabBrown.addEventListener("mousedown", function(e){
+		curColor_simpleColors = colorBrown;
+	});
 	//add Eraser
 	clickEraser.addEventListener("mousedown", function(e){
 		curColor_simpleColors = eraser;
@@ -113,21 +137,31 @@ function prepareSimpleCanvas()
 	pickLarge.addEventListener("mousedown", function(e){
 		curSize = "large";
 	});
+	//undo and redo events
+	redoButton.addEventListener("mousedown", function(e){
+		redo();
+
+	});
+	undoButton.addEventListener("mousedown", function(){
+		undo();
+		console.log("undo");
+	});
 	// Add touch event listeners to canvas element
 	canvas_simple.addEventListener("touchstart", function(e)
 	{
+		var pos = getMousePos(canvasDiv,e);
 		// Mouse down location
-		var mouseX = (e.changedTouches ? e.changedTouches[0].pageX : e.pageX) - this.offsetLeft,
-			mouseY = (e.changedTouches ? e.changedTouches[0].pageY : e.pageY) - this.offsetTop;
+		var mouseX = (e.changedTouches ? e.changedTouches[0].pageX : pos.x) - this.offsetLeft,
+			mouseY = (e.changedTouches ? e.changedTouches[0].pageY : pos.y) - this.offsetTop;
 
 		paint_simple = true;
 		addClickSimple(mouseX, mouseY, false);
 		redrawSimple();
 	}, false);
 	canvas_simple.addEventListener("touchmove", function(e){
-
-		var mouseX = (e.changedTouches ? e.changedTouches[0].pageX : e.pageX) - this.offsetLeft,
-			mouseY = (e.changedTouches ? e.changedTouches[0].pageY : e.pageY) - this.offsetTop;
+		var pos = getMousePos(canvasDiv,e);
+		var mouseX = (e.changedTouches ? e.changedTouches[0].pageX : pos.x) - this.offsetLeft,
+			mouseY = (e.changedTouches ? e.changedTouches[0].pageY : pos.y) - this.offsetTop;
 
 		if(paint_simple){
 			addClickSimple(mouseX, mouseY, true);
@@ -144,6 +178,68 @@ function prepareSimpleCanvas()
 	}, false);
 }
 
+
+
+    function saveImage() {
+		//save the canvas image to undo array
+          var imgSrc = canvas_simple.toDataURL("image/png");
+          savedImages.push(imgSrc);
+          //undoButton.enable();
+    }
+    function undo() {
+		console.log('Undo CANVAS button clicked!');
+		undoLog=1;
+		redoLog=0;
+          //save the current canvas in redo array
+          this.removeStrokes();
+          redrawSimple();
+
+
+    }
+    function redo() {
+		console.log('Redo CANVAS button clicked!');
+		redoLog=1;
+		undoLog=0;
+          //save the current canvas in undo array
+          this.surrateStroke();
+          //redraw the canvas
+
+		  redrawSimple();
+
+    }
+    function removeImage() {
+		//save the canvas image to redo array
+
+          var imgSrc = canvas_simple.toDataURL("image/png");
+          removedImages.push(imgSrc);
+          //redoButton.enable();
+    }
+	function removeStrokes() {
+		//save the canvas image to redo array
+		removeX=[];
+		removeY=[];
+		removeX=strokex;
+		removeY=strokey;
+
+
+          var imgSrc = canvas_simple.toDataURL("image/png");
+          removedImages.push(imgSrc);
+          //redoButton.enable();
+    }
+	function surrateStroke(){
+		for (var i=0; i<removeX.length;i++){
+			addClickSimple(removeX[i],removeY[i],true)
+		}
+		strokex.push(removeX);
+		strokey.push(removeY);
+	}
+
+
+function saveStroke(x,y){
+	strokex.push(x);
+	strokey.push(y);
+
+}
 function addClickSimple(x, y, dragging)
 {
 	clickX_simple.push(x);
@@ -161,7 +257,19 @@ function clearCanvas_simple()
 function redrawSimple()
 {
 	clearCanvas_simple();
-
+	if(undoLog == 1){
+		for(var i=0; i < clickX_simple.length; i++){
+			//if(clickDrag_simple[i] && i){
+				for(var k=0; k < strokex.length; k++){
+					if(clickX_simple[i] == strokex[k] && clickY_simple[i] == strokey[k] ){
+						delete clickX_simple[i];
+						delete clickY_simple[i];
+					}
+				}
+			//}
+		}
+		undoLog = 0;
+	}
 	var radius;
 	//context_simple.strokeStyle = "#df4b26";
 	context_simple.lineJoin = "round";
